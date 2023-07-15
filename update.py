a0842068be9f7ee68cdec717b09e605d90609959
Arameair <dustinmgiff@gmail.com>
2023-07-15 05:30:43 +0000

    On branch main  Your branch is up to date with 'origin/main'. 	modified:   env 	modified:   update.py

--- update.py
+++ update.py
@@ -4,50 +4,49 @@ from dotenv import load_dotenv
 import subprocess
 
 def run_command(command):
-    process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
-    output, error = process.communicate()
-    if error:
-        print(f"Error: {error}")
-    return output
+  process = subprocess.Popen(command.split(), stdout=subprocess.PIPE)
+  output, error = process.communicate()
+  if error:
+    print(f"Error: {error}")
+  return output
 
-def update_github():
-    # Load environment variables from .env file
-    load_dotenv()
+def init_github_repo():
 
-    # Check if the current directory is a Git repository
-    if not os.path.exists('.git'):
-        print("Git repository has not been initialized. Initializing now...")
-        run_command('git init')
+  # Load env variables
+  load_dotenv()
+  github_token = os.getenv('GITHUB_TOKEN')
 
-    # Get the GitHub token from the environment variables
-    github_token = os.getenv('GITHUB_TOKEN')
-    if not github_token:
-        print("GitHub token not found in .env file.")
-        return
+  if not github_token:
+    print("GitHub token not found")
+    return
 
-    # Prompt the user for the necessary information
-    repo_name = input("Enter the name of the GitHub repository: ")
-    commit_message = input("Enter a commit message: ")
+  # Create GitHub object
+  g = Github(github_token)
 
-    # Create a GitHub object using the provided token
-    g = Github(github_token)
+  # Get user
+  user = g.get_user()
 
-    # Get the user
-    user = g.get_user()
+  # Set repo name
+  repo_name = "basicGoldBot"
 
-    # Create a new repository if it doesn't exist
-    if repo_name not in [repo.name for repo in user.get_repos()]:
-        user.create_repo(repo_name)
+  # Create README
+  with open("README.md", "w") as f:
+    f.write("# "+repo_name)
 
-    # Add all the files in the current directory to the staging area
-    run_command('git add .')
+  # Initialize Git repo
+  run_command("git init")
 
-    # Commit the changes
-    run_command(f'git commit -m "{commit_message}"')
+  # Add and commit README
+  run_command("git add README.md")
+  run_command('git commit -m "first commit"')
 
-    # Push the changes to GitHub
+  # Check if remote exists, if not, add it
+  remotes = run_command("git remote").decode().split("\n")
+  if "origin" not in remotes:
     run_command(f'git remote add origin https://github.com/{user.login}/{repo_name}.git')
-    run_command('git push -u origin master')
+
+  # Push to GitHub
+  run_command("git push -u origin master")
 
 if __name__ == "__main__":
-    update_github()
+  init_github_repo()
